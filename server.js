35ee676604a0bff2afb88c41aaf9b279cda374d1
mohan chinnappan <mohan.chinnappan.n@gmail.com>
2023-12-20 09:52:14 -0500

    cs added

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     request = require('request'),
     qrcode = require('qrcode-npm'),
-    decode = require('salesforce-signed-request'),
+    decode = require('salesforce-signed-request');
 
-    consumerSecret = process.env.CONSUMER_SECRET,
+    const consumerSecret = process.env.CONSUMER_SECRET || '7EA1B713786B319BEBE18F2C375B9208F077404AA35D4C30301AC1C0D3E688C7';
 
     app = express();
 
@@ -20,28 +20,34 @@ app.post('/signedrequest', function(req, res) {
         oauthToken = signedRequest.client.oauthToken,
         instanceUrl = signedRequest.client.instanceUrl,
 
-        query = "SELECT Id, FirstName, LastName, Phone, Email FROM Contact WHERE Id = '" + context.environment.record.Id + "'",
-
-        contactRequest = {
-            url: instanceUrl + '/services/data/v29.0/query?q=' + query,
+        query = "SELECT Id, FirstName, LastName, Phone, Email FROM Contact WHERE Id = '" + context.environment.record.Id + "'";
+        console.log(oauthToken)
+        console.log(context.environment.record.Id);
+        const contactRequest = {
+            url: instanceUrl + '/services/data/v59.0/query?q=' + query,
             headers: {
                 'Authorization': 'OAuth ' + oauthToken
             }
         };
 
+        console.log(contactRequest);
+
     request(contactRequest, function(err, response, body) {
-        var qr = qrcode.qrcode(4, 'L'),
-            contact = JSON.parse(body).records[0],
-            text = 'MECARD:N:' + contact.LastName + ',' + contact.FirstName + ';TEL:' + contact.Phone + ';EMAIL:' + contact.Email + ';;';
-        qr.addData(text);
+        let qr = qrcode.qrcode(4, 'L'),
+        contact = JSON.parse(body).records[0];
+        console.log(contact);
+         let text = 'MECARD:N:' + contact.LastName + ',' + contact.FirstName + ';TEL:' + contact.Phone + ';EMAIL:' + contact.Email + ';;';
+         qr.addData(text);
         qr.make();
-        var imgTag = qr.createImgTag(4);
+        const imgTag = qr.createImgTag(4);
+        //res.render('index', {context: context});
         res.render('index', {context: context, imgTag: imgTag});
+
     });
 
 });
 
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 8000);
 
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
